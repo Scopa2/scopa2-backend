d290634e442f2a22184e3c60fefabeab3e73a4e9
Marco Coppola <marcocoppola02.mc@gmail.com>
2026-04-16 14:58:28 +0200

    Heartbeat event and notes on known bugs

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\HeartBeat;
 use App\Jobs\ProcessMatchmakingQueue;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -10,3 +11,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::job(new ProcessMatchmakingQueue)->everySecond()->onOneServer();
+Schedule::job(new HeartBeat())->everyFiveSeconds()->onOneServer();
